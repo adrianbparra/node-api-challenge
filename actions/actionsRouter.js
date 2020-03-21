7701d36223ebe4c0321bb9a8e82cb5834f87b2e8
adrianbparra <adrianplane12@gmail.com>
2020-03-21 10:39:24 -0700

    added the projects server methods

---
+++ actions/actionsRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const actionsRouter = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = actionsRouter
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const projectsRouter = require("./projects/projectsRouter.js")
+const actionRouter = require("./actions/actionsRouter.js")
 
 const server = express()
 
@@ -11,9 +13,17 @@ server.use(helmet());
 server.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 
+
+
+
 server.get("/", (req,res)=>{
     res.send(`Welcome to Adrian's Server!`)
 })
+
+
+server.use("/api/projects", projectsRouter)
+server.use("/api/actions", actionRouter)
+
 
 
 
